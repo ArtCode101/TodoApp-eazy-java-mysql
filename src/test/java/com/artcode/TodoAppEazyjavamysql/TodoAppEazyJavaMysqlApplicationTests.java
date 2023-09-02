b1e613e60e77092f3421ab56c1bff096a6bd2ad0
ArtCode101 <sanayut12.service@gmail.com>
2023-09-03 00:59:57 +0700

    setup project

---
+++ src/test/java/com/artcode/TodoAppEazyjavamysql/TodoAppEazyJavaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artcode.TodoAppEazyjavamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppEazyJavaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
